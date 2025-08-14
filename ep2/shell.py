6176d21573d967f96734e2f01be3a14d31356f6e
DynamoDynamo <vijayagprec3b7@gmail.com>
2025-08-13 20:31:11 -0400

    POD1-193 ep2 new folder

---
+++ ep2/shell.py
@@ -0,0 +1,9 @@
+
+import basic
+
+while True:
+    text = input('basic >') #input(variable) takes input from the user
+    result, error = basic.run('<stdin>', text)
+
+    if error: print(error.as_string())
+    else: print(result)
