c1d1b58ca2aa321a30e520f5bdcc9d68f58ea334
DynamoDynamo <vijayagprec3b7@gmail.com>
2026-07-27 20:47:07 -0400

    research

--- ep1/shell.py
+++
@@ -1,9 +0,0 @@
-
-import basic
-
-while True:
-    text = input('basic >') #input(variable) takes input from the user
-    result, error = basic.run('<stdin>', text)
-
-    if error: print(error.as_string())
-    else: print(result)
